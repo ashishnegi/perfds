77a1d50903ceb681529630d40888363965a982a8
Ashish Negi <asnegi@microsoft.com>
2020-03-14 16:25:17 -0700

    Failure test case: LinkedList is not supported to be concurrent safe.. Although i am only using AddFront

--- tests/SnapshotSingleWriterDictionaryTests.cs
+++ tests/SnapshotSingleWriterDictionaryTests.cs
@@ -1,7 +1,10 @@
 using FsCheck;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PerfDS
 {
@@ -60,27 +63,108 @@ namespace PerfDS
                 }
             }).QuickCheckThrowOnFailure();
         }
+
+        [TestMethod]
+        public void SSWDReadSameKeyMultipleValues()
+        {
+            Prop.ForAll<int[]>(xs =>
+            {
+                var dict = new ReaderSnapshotSingleWriterDictionary<int, int>();
+
+                Array.Sort(xs);
+
+                int lastValue = -1;
+                foreach (var x in xs)
+                {
+                    if (lastValue == x)
+                    {
+                        continue;
+                    }
+
+                    lastValue = x;
+
+                    // we should not already have this value.
+                    int actualValue = 0;
+                    if (dict.TryGetValue(1, out actualValue))
+                    {
+                        Assert.AreNotEqual(x, actualValue);
+                    }
+
+                    dict.Add(1, x);
+
+                    // we should have this value.
+                    Assert.IsTrue(dict.TryGetValue(1, out actualValue));
+                    Assert.AreEqual(x, actualValue);
+                }
+            }).QuickCheckThrowOnFailure();
+        }
     }
 
-    // [TestClass]
-    // public class SnapshotSingleWriterDictionaryTestsMultiThread
-    // {
-    //     [TestMethod]
-    //     public void SSWDLatestDataAvailableAfterWrite()
-    //     {
-    //         Prop.ForAll<Tuple<int, int>[]>(xs =>
-    //         {
+    [TestClass]
+    public class SnapshotSingleWriterDictionaryTestsMultiThread
+    {
+        [TestMethod]
+        public void SSWDLatestDataAvailableAfterWrite()
+        {
+            Prop.ForAll<int[]>(xs =>
+            {
+                var dict = new ReaderSnapshotSingleWriterDictionary<int, int>();
 
-    //             var dict = new ReaderSnapshotSingleWriterDictionary<int, int>();
+                Array.Sort(xs);
 
-    //             foreach (var x in xs)
-    //             {
-    //                 dict.Add(x.Item1, x.Item2);
-    //                 int actualValue = 0;
-    //                 Assert.IsTrue(dict.TryGetValue(x.Item1, out actualValue));
-    //                 Assert.AreEqual(x.Item2, actualValue);
-    //             }
-    //         }).QuickCheckThrowOnFailure();
-    //     }
-    // }
+                var writerTask = new Task(() => {
+                    foreach (var x in xs)
+                    {
+                        dict.Add(1, x);
+                        int actualValue = 0;
+                        Assert.IsTrue(dict.TryGetValue(1, out actualValue));
+                        Assert.AreEqual(x, actualValue);
+                    }
+                });
+
+                Action readerFn = () => {
+                    var lastValueSeen = -1;
+                    if (xs.Length > 0)
+                    {
+                        lastValueSeen = xs[0];
+                    }
+
+                    foreach (var x in xs)
+                    {
+                        int actualValue = 0;
+                        if (dict.TryGetValue(1, out actualValue))
+                        {
+                            // every reader should see value in increasing order.
+                            if (lastValueSeen > actualValue)
+                            {
+                                Assert.Fail("Reader thread");
+                            }
+
+                            lastValueSeen = actualValue;
+                        }
+                    }
+                };
+
+                var allTasks = new List<Task>(10);
+                for (int i = 0; i < 4; ++i)
+                {
+                    allTasks.Add(new Task(readerFn));
+                }
+
+                allTasks.Add(writerTask);
+
+                for (int i = 0; i < 5; ++i)
+                {
+                    allTasks.Add(new Task(readerFn));
+                }
+
+                foreach (var t in allTasks)
+                {
+                    t.Start();
+                }
+
+                Task.WaitAll(allTasks.ToArray());
+            }).QuickCheckThrowOnFailure();
+        }
+    }
 }
