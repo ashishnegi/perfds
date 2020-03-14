b17922ed6a86fb379a9c95febdefc96d36928d46
Ashish Negi <asnegi@microsoft.com>
2020-03-14 14:45:27 -0700

    moved benchmark to dotnet core

--- Benchmark/Benchmark.cs
+++ Benchmark/Benchmark.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Engines;
 
-namespace partitionedlist
+namespace PerfDS
 {
     public class ListCompare
     {
--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -1,6 +1,6 @@
 ﻿using BenchmarkDotNet.Running;
 
-namespace partitionedlist
+namespace PerfDS
 {
     public class Program
     {
